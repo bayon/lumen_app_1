f01823bdfbaf87e99c16c43ca35e05a847bf324a
Bayon Forte <bayon@forteworks.com>
2018-09-01 10:38:46 -0400

    completed the seeding code and had to update to php7 inorder to run composer install

--- blog/database/seeds/DatabaseSeeder.php
+++ blog/database/seeds/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Teacher;
+use App\Student;
+use App\Course;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,9 +22,13 @@ class DatabaseSeeder extends Seeder
         Student::truncate();
         Course::truncate();
         // LEFT OFF HERE....DB::Course_Table
-        
+        DB::table('course_student')->truncate();
+
+
         factory(Teacher::class, 50)->create();
         factory(Student::class, 500)->create();
+        //need to add students randomly..
+        //attach() method receives an array...we chose 40 random from 1 to 500s
         factory(Course::class, 40 )->create()->each(function($course){
             $course->students()->attach(array_rand(range(1,500),40));
         });
